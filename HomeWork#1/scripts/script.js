5ab1e9fa2bdd8084e47e831273d4045ac483a333
Yaroslav Golub <yartes83@gmail.com>
2022-08-21 13:27:59 +0300

    21.08.2022

--- HomeWork#1/scripts/script.js
+++ HomeWork#1/scripts/script.js
@@ -4,50 +4,60 @@ const pricePie = 123.965;
 
 //Використовуючи вбудований об'єкт Math – виведіть максимальне число:
 console.log("Максимальне число:" + (Number(Math.max(priceSweetmeats, priceIceCream, pricePie))));
-document.writeln("Максимальне число: " + Number(Math.max(priceSweetmeats, priceIceCream, pricePie)) + "<br>" + "<br>");
+document.writeln("Максимальне число: " + Number(Math.max(priceSweetmeats, priceIceCream, pricePie)) + " грн" + "<br>" + "<br>");
 
 
 //Використовуючи вбудований об'єкт Math – виведіть мінімальне число:
 console.log("Мінімальне число: " + (Number(Math.min(priceSweetmeats, priceIceCream, pricePie))));
-document.writeln("Мінімальне число: " + Number(Math.min(priceSweetmeats, priceIceCream, pricePie)) + "<br>" + "<br>");
+document.writeln("Мінімальне число: " + Number(Math.min(priceSweetmeats, priceIceCream, pricePie)) + " грн" + "<br>" + "<br>");
 
 
 //Складіть вартість всіх товарів, помістіть її в змінну та виведіть цю суму:
 let priceSum = priceSweetmeats + priceIceCream + pricePie;
 console.log("Вартість всіх товарів: " + (Number(priceSum)));
-document.writeln("Вартість всіх товарів: " + priceSum + "<br>" + "<br>");
+document.writeln("Вартість всіх товарів: " + priceSum + " грн" + "<br>" + "<br>");
 
 
 //Відкиньте копійки у всіх товарів, потім – складіть цілу частину вартості кожного товару між собою. Округлення використовувати в МЕНЬШУ сторону:
 console.log("Округлення суми в меньшу сторону: " + (Math.floor(priceSweetmeats) + Math.floor(priceIceCream) + Math.floor(pricePie)));
 console.log("Округлення суми в меньшу сторону(parseInt): " +  (parseInt(priceSweetmeats) + parseInt(priceIceCream) + parseInt(pricePie))); //мабуть так не можна, нагуглив щось таке. Проте результат такий самий. Це через велику терпимість JS до кривого коду?
-document.writeln("Округлення суми в меньшу сторону: " + (Math.floor(priceSweetmeats) + Math.floor(priceIceCream) + Math.floor(pricePie)) + "<br>" + "<br>" );
+document.writeln("Округлення суми в меньшу сторону: " + (Math.floor(priceSweetmeats) + Math.floor(priceIceCream) + Math.floor(pricePie)) + " грн" + "<br>" + "<br>" );
 
 
 //Виведіть суму товарів округлену до сотень. (Наприклад якщо вийшло 260, то виведіть 300):
 console.log("Сума товарів, округлена до сотень: " + (Math.ceil((priceSweetmeats + priceIceCream + pricePie) / 100.0) * 100.0)); //нагуглив :)
-
+document.writeln("Сума товарів округлена до сотень: " + (Math.ceil((priceSweetmeats + priceIceCream + pricePie) / 100.0) * 100.0) + " грн" + "<br>" + "<br>" );
 
 
 //Виведіть булеве значення: чи є сума всіх товарів (округлена в меншу сторону) парним чи непарним числом?
 if ((Math.floor(priceSweetmeats) + Math.floor(priceIceCream) + Math.floor(pricePie)) % 2 == 0) {
-    console.log("парне");
+    console.log("Сума товарів: парна");
+    document.writeln("Сума товарів: парна" + "<br>" + "<br>");
 }
 else {
-    console.log("не парне");
+    console.log("Сума товарів: не парна");
+    document.writeln("Сума товарів: не парна" + "<br>" + "<br>");
 }
 
 //Виведіть суму решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн:
-console.log(Math.abs((priceSweetmeats + priceIceCream + pricePie)  - 500));
+console.log(("Сума решти, при оплаті всіх товарів, якщо клієнт платить 500 грн: " + Math.abs((priceSweetmeats + priceIceCream + pricePie) - 500)) + " грн");
+document.writeln("Сума решти, при оплаті всіх товарів, якщо клієнт платить 500 грн: " + Math.abs((priceSweetmeats + priceIceCream + pricePie)  - 500) + " грн" + "<br>" + "<br>")
 
 //Виведіть середнє значення цін, округлене до другого знаку після коми:
-console.log(Number(
-    Math.round((priceSweetmeats + priceIceCream + pricePie).toFixed(2))
-    )
+console.log("Cереднє значення цін, округлене до другого знаку після коми:" + Number(
+    Math.round((priceSweetmeats + priceIceCream + pricePie)) / 3
+    ).toFixed(2)
 )
+document.writeln("Cереднє значення цін, округлене до другого знаку після коми:" + Number(
+    Math.round((priceSweetmeats + priceIceCream + pricePie)) / 3
+).toFixed(2) + " грн" + "<br>" + "<br>"
+)
+
+
 //Створіть змінну, в якої збережіть випадкову знижку:
 const randomValue = ((100 - 1) * Math.random() + 1).toFixed(0);
 console.log("випадкова знижка: " + randomValue);
+document.writeln("Випадкова знижка: " + randomValue + "%" + "<br>" + "<br>");
 
 //Зробіть клієнту випадкову знижку та виведіть суму до оплати округлену до 2 знаків після коми:
 console.log("сума до сплати: " +
@@ -55,19 +65,32 @@ console.log("сума до сплати: " +
         (priceSweetmeats + priceIceCream + pricePie) - (((priceSweetmeats + priceIceCream + pricePie) / 100 * randomValue).toFixed(2))
     ).toFixed(2)
 )
+document.writeln("Сума до сплати: " +
+    (
+        (priceSweetmeats + priceIceCream + pricePie) - (((priceSweetmeats + priceIceCream + pricePie) / 100 * randomValue).toFixed(2))
+    ).toFixed(2) + " грн" + "<br>" + "<br>"
+)
 
 //Виведіть чистий прибуток, якщо клієнт заплатив зі знижкою та собівартість товарів рівно в два рази нижче їх ціни:
 const totalPrice = priceSweetmeats + priceIceCream + pricePie; //загальна вартість
 console.log("загальна вартість: " + totalPrice);
+document.writeln("Загальна вартість: " + totalPrice + " грн" + "<br>" + "<br>")
+
 const costPrice = totalPrice / 2; //собівартість
 console.log("собівартість: " + costPrice);
+document.writeln("Собівартість: " + costPrice + " грн" + "<br>" + "<br>");
+
 const clientPay = ((priceSweetmeats + priceIceCream + pricePie) - (((priceSweetmeats + priceIceCream + pricePie) / 100 * randomValue).toFixed(2))).toFixed(2);
 console.log("клієнт заплатив: " + clientPay);
+document.writeln("Клієнт заплатив: " + clientPay + " грн" + "<br>" + "<br>");
+
 const howMuch = (totalPrice - clientPay).toFixed(3);
 console.log("на скільки менше заплатив: " + howMuch);
+document.writeln("На скільки менше заплатив: " + howMuch + " грн" + "<br>" + "<br>");
 
 const profit = (costPrice - howMuch).toFixed(3); //чистий прибуток
 console.log("чистий прибуток: " + profit);
+document.writeln("Чистий прибуток: " + profit + " грн" + "<br>" + "<br>");
 
 
 
