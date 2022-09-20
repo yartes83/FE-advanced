99addf5905b4893ae8550f4c661bb4e80fa6f937
Yaroslav Golub <yartes83@gmail.com>
2022-09-20 20:24:21 +0300

    HomeWork_8

--- HomeWork_8/scripts/script.js
+++ HomeWork_8/scripts/script.js
@@ -11,6 +11,13 @@ class Book {
 let book1 = new Book("Муму","Тургенев", 112);
 console.log(book1)
 
+//аргументи класу
 for (let p in book1) {
     console.log(p+": "+book1[p]);
+}
+
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+
+for (let p in ukraine) {
+    console.log(p+": "+ukraine[p])
 }
