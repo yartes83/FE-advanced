29d5c6b1c9372fc1318bfa992c8d0f5ffb1e7d20
Yaroslav Golub <yartes83@gmail.com>
2022-10-15 08:37:53 +0300

    lecture12

--- lecture_12/scripts/script.js
+++ lecture_12/scripts/script.js
@@ -4,7 +4,96 @@
  //Синтаксис для скасування:
  // let timerId = setTimeout();
  // clearTimeout(timerId);
- let timerId = setTimeout(() => alert("ніколи не відбувається"), 10000);
- alert(timerId);
- clearTimeout(timerId);
- alert(timerId);
+ // let timerId = setTimeout(() => alert("ніколи не відбувається"), 10000);
+ // alert(timerId);
+ // clearTimeout(timerId);
+ // alert(timerId);
+
+ // setTimeout(() => {
+ //  console.log('hello');
+ // }, 5000);
+ //
+//  let timerId = setInterval(() => {
+//   console.log('a');
+//  }, 1000);
+//
+//  console.log({timerId});
+//
+//  const button1 = document.getElementById('enable');
+//  const button2 = document.getElementById('disable');
+//
+// button1.onclick = () => {
+//  let timerId = setInterval(() => {
+//   console.log('б');
+//  }, 1000);
+// }
+//
+// button2.onclick = () => {
+//  clearInterval(timerId);
+// }
+function sayHi(phrase, who) {
+  alert(phrase + ', ' + who);
+}
+//setTimeout(sayHi, 3000, 'Hello', 'amigo');
+
+//setTimeout(alert ('кукуся'), 5000) говнокод
+
+let timerId = setTimeout(sayHi, 3000, 'hello', 'amigo');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
