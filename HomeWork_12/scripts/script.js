70c0e775bfeeaa104610df34226f74c35f7aa26b
Yaroslav Golub <yartes83@gmail.com>
2022-10-25 14:54:55 +0300

    HomeWork_12

--- HomeWork_12/scripts/script.js
+++ HomeWork_12/scripts/script.js
@@ -1,48 +1,25 @@
-function pause(audio) {
-    if (!audio) return;
+const getRandomChinese = async (length) => {
+    try {
+        let result = '';
+        let sign;
+        for (let i = 0; i < length; i++) {
+            sign = Date.now().toString().slice(-5);
+            result += String.fromCharCode(sign);
+            await waiting();
+        }
+        console.log(result);
+        return result;
 
-    audio.setAttribute('data-isPlaying', false);
-    audio.pause();
-    audio.currentTime = 0;
-}
-
-function play(audio) {
-    if (!audio) return;
-
-    audio.setAttribute('data-isPlaying', true);
-    audio.play();
-}
-
-function toggle(audio) {
-    const playingAudio = document.querySelector('[data-isPlaying=true]');
-
-    if (!!playingAudio) {
-        pause(playingAudio);
-        if (playingAudio !== audio) play(audio);
-    } else play(audio);
-}
-
-function onClick(event) {
-    const targetDataName = event.target.getAttribute("data-name");
-    const audio = document.querySelector(`[data-${targetDataName}audio]`);
-
-    if (!audio) return;
-
-    toggle(audio);
-}
-
-function onEnded(event){
-    event.target.setAttribute('data-isPlaying', false);
-}
-
-function setEventListeners () {
-    for (const button of document.getElementsByClassName('button')) {
-        button.addEventListener('click', onClick);
-    }
-
-    for (const audio of document.getElementsByClassName('audio')) {
-        audio.addEventListener('ended', onEnded);
+    }catch (error) {
+        console.log('Omg, error: ', result);
     }
 }
 
-setEventListeners();
+function waiting() {
+    return new Promise(resolve => setTimeout(resolve, 50));
+}
+
+getRandomChinese(10);
+
+
+
